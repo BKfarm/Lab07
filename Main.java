19e150c23d2b8eda5145eb1d97c40fb6c97f5a1f
Ben Fahrmeier <144169391+BKfarm@users.noreply.github.com>
2023-10-15 12:39:34 -0400

    finish the lab
    add all the loops

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
@@ -18,6 +20,47 @@ public class Main
         {
             System.out.println(x);
         }
+
+        for (int x = 0; x < 5; x++)
+        {
+            for (int y = 0; y < (x+1); y++)
+            {
+            System.out.print("*");
+            }
+            System.out.println();
+        }
+                for (int x = 0; x < 5; x++)
+        {
+            for (int y = 5; (y+1) > (x+1); y--)
+            {
+            System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int x = 0; x < 5; x++)
+        {
+            for (int y = 0; y < 5; y++)
+            {
+            System.out.print("*");
+            }
+            System.out.println();
+        }
+
+            boolean done = false;
+        double startTemp = 0;
+        Scanner in = new Scanner(System.in);
+        do
+        {
+        System.out.println("Input temperature in Celsius: ");
+        if(in.hasNextDouble())
+        {
+            startTemp = in.nextDouble();
+            done = true;
+        }
+        in.nextLine();
+        }while(!done);
+        startTemp = (startTemp * 9/5) + 32;
+        System.out.println("Temperature in Fahrenheit: " + startTemp);
     }
 
 }
